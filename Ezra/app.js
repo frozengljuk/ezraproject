ced018fd45a0e4721ffa956093d43d99e1d9a7e0
Alex <lex.volkow@gmail.com>
2020-04-18 19:19:49 +0300

    Added get_by_id and add_user requests

--- Ezra/app.js
+++ Ezra/app.js
@@ -42,6 +42,55 @@ app.get("/api/users", (req, res, next) => {
     });
 });
 
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ "error": err.message });
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": row
+        })
+    });
+});
+
+app.post("/api/user/", (req, res, next) => {
+    var errors = []
+    if (!req.body.password) {
+        errors.push("No password specified");
+    }
+    if (!req.body.email) {
+        errors.push("No email specified");
+    }
+    if (errors.length) {
+        res.status(400).json({ "error": errors.join(",") });
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password: md5(req.body.password)
+    }
+    var sql = 'INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params = [data.name, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ "error": err.message })
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id": this.lastID
+        })
+    });
+})
+
+
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
