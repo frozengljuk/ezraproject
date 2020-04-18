69c5dcb21a39b417f370846b7254ae037b7102d9
Alex <lex.volkow@gmail.com>
2020-04-18 19:07:12 +0300

    Added Index

--- Ezra/app.js
+++ Ezra/app.js
@@ -7,15 +7,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
 
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -26,11 +19,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'wwwroot')));
 
-//app.use('/', routes);
-//app.use('/users', users);
 
 app.get("/", (req, res, next) => {
-    res.redirect('/home.html')
+    res.redirect('/index.html')
+});
+
+app.get("/index", (req, res, next) => {
+    res.redirect('/index.html')
 });
 
 // catch 404 and forward to error handler
@@ -41,26 +36,11 @@ app.use(function (req, res, next) {
 });
 
 // error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
 app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: err
     });
 });
 
